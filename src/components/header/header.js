98beba3e381ff68fe75617191396129d016e1174
temfa <topeakinfe@gmail.com>
2022-08-21 07:24:27 +0100

    Fixed dropdown

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,255 +9,259 @@ import { ref, onValue } from "firebase/database";
 import styled from "styled-components";
 
 const Header = ({ color }) => {
-	const HeaderStyle = styled.div`
-		display: flex;
-		width: 100%;
-		justify-content: space-between;
-		align-items: center;
+  const HeaderStyle = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
 
-		.header-nav {
-			display: flex;
-			justify-content: space-between;
-			width: 30%;
-		}
+    .header-nav {
+      display: flex;
+      justify-content: space-between;
+      width: 30%;
+    }
 
-		.header-logo img {
-			width: 50%;
-		}
+    .header-logo img {
+      width: 50%;
+    }
 
-		.header-nav a {
-			color: ${({ color }) => color};
-			font-size: 17px;
-			font-weight: 300;
-			font-family: "Roboto", sans-serif;
-			line-height: 23px;
-			text-decoration: none;
-			transition: 1s all ease;
-		}
+    .header-nav a {
+      color: ${({ color }) => color};
+      font-size: 17px;
+      font-weight: 300;
+      font-family: "Roboto", sans-serif;
+      line-height: 23px;
+      text-decoration: none;
+      transition: 1s all ease;
+    }
 
-		.header-nav a:hover {
-			color: #bc9144;
-			transform: scale(1.2);
-		}
+    .header-nav a:hover {
+      color: #bc9144;
+      transform: scale(1.2);
+    }
 
-		.header-nav .active {
-			font-weight: 900;
-		}
+    .header-nav .active {
+      font-weight: 900;
+    }
 
-		.active button {
-			border: 1px solid #bc9144;
-			background-color: #0b0728;
-		}
+    .active button {
+      border: 1px solid #bc9144;
+      background-color: #0b0728;
+    }
 
-		.nav-links-mobile {
-			display: none;
-		}
-		.menu-icon {
-			color: ${({ color }) => color};
-			cursor: pointer;
-			font-size: 30px;
-			display: none;
-		}
+    .nav-links-mobile {
+      display: none;
+    }
+    .menu-icon {
+      color: ${({ color }) => color};
+      cursor: pointer;
+      font-size: 30px;
+      display: none;
+    }
 
-		.dropdown {
-			position: relative;
-		}
+    .dropdown {
+      position: relative;
+    }
 
-		.dropdown-menu {
-			width: 200px;
-			background-color: #bc9144;
-			border-radius: 10px;
-			position: absolute;
-			top: 20px;
-			left: 0px;
-			z-index: 999;
-		}
+    .dropdown-menu {
+      width: 200px;
+      background-color: #bc9144;
+      border-radius: 10px;
+      position: absolute;
+      top: 20px;
+      left: 0px;
+      z-index: 999;
+    }
 
-		.dropdown-menu a {
-			display: block;
-			padding: 19px 20px;
-			color: white;
-			font-size: 17px;
-			font-weight: 300;
-			font-family: "Roboto", sans-serif;
-			line-height: 23px;
-			text-decoration: none;
-			transition: 1s all ease;
-		}
+    .dropdown-menu a {
+      display: block;
+      padding: 19px 20px;
+      color: white;
+      font-size: 17px;
+      font-weight: 300;
+      font-family: "Roboto", sans-serif;
+      line-height: 23px;
+      text-decoration: none;
+      transition: 1s all ease;
+    }
 
-		.dropdown-menu a:hover {
-			color: white;
-		}
-		@media screen and (max-width: 1050px) {
-			position: relative;
+    .dropdown-menu a:hover {
+      color: white;
+    }
+    @media screen and (max-width: 1050px) {
+      position: relative;
 
-			.header-logo {
-				display: flex;
-				width: 100%;
-				justify-content: space-between;
-				align-items: center;
-			}
+      .header-logo {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        align-items: center;
+      }
 
-			.header-nav {
-				display: flex;
-				flex-direction: column;
-				width: 100%;
-				position: absolute;
-				top: 130px;
-				left: -150%;
-				opacity: 1;
-				transition: all 2s ease-in-out;
-				padding: 30px 0px;
-			}
+      .header-nav {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        position: absolute;
+        top: 130px;
+        left: -150%;
+        opacity: 1;
+        transition: all 2s ease-in-out;
+        padding: 30px 0px;
+      }
 
-			.header-nav a {
-				text-align: center;
-				padding: 20px 0px;
-				color: white;
-			}
+      .header-nav a {
+        text-align: center;
+        padding: 20px 0px;
+        color: white;
+      }
 
-			.header-nav.actived {
-				background: rgb(36, 34, 34);
-				left: 0;
-				opacity: 1;
-				transition: all 1s ease-in-out;
-				z-index: 1111111111;
-				margin-left: 0px;
-				width: 100%;
-			}
+      .header-nav.actived {
+        background: rgb(36, 34, 34);
+        left: 0;
+        opacity: 1;
+        transition: all 1s ease-in-out;
+        z-index: 1111111111;
+        margin-left: 0px;
+        width: 100%;
+      }
 
-			.dropdown {
-				width: 100%;
-				text-align: center;
-				padding: 20px 0px;
-			}
+      .dropdown {
+        width: 100%;
+        text-align: center;
+        padding: 20px 0px;
+      }
 
-			.dropdown-menu {
-				top: 40px;
-			}
+      .dropdown-menu {
+        top: 40px;
+      }
 
-			.nav-links-mobile {
-				display: block;
-				margin-top: 20px;
-			}
+      .nav-links-mobile {
+        display: block;
+        margin-top: 20px;
+      }
 
-			.nav-links {
-				display: none;
-			}
+      .nav-links {
+        display: none;
+      }
 
-			.menu-icon {
-				display: block;
-			}
+      .menu-icon {
+        display: block;
+      }
 
-			.dropdown-menu {
-				width: 100%;
-			}
-		}
-	`;
-	const [click, setClick] = useState(false);
-	const [dropdown, setDropdown] = useState(false);
-	const handleClick = () => setClick(!click);
-	const closeMobileMenu = () => setClick(false);
-	const [propertyData, setPropertyData] = useState("");
-	const getPropertyDetail = () => {
-		onValue(ref(db), (snapshot) => {
-			const data = snapshot.val();
-			if (data !== null) {
-				setPropertyData(Object.values(data.newProperty));
-			}
-		});
-	};
-	const onMouseEnter = () => {
-		if (window.innerWidth > 1500) {
-			setDropdown(false);
-		} else {
-			setDropdown(true);
-		}
-	};
+      .dropdown-menu {
+        width: 100%;
+      }
+    }
+  `;
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const [propertyData, setPropertyData] = useState("");
+  const getPropertyDetail = () => {
+    onValue(ref(db), (snapshot) => {
+      const data = snapshot.val();
+      if (data !== null) {
+        setPropertyData(Object.values(data.newProperty));
+      }
+    });
+  };
+  console.log(window.innerWidth);
+  const onMouseEnter = () => {
+    if (window.innerWidth > 1600) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
 
-	const onMouseLeave = () => {
-		if (window.innerWidth > 1500) {
-			setDropdown(false);
-		} else {
-			setDropdown(false);
-		}
-	};
-	useEffect(() => {
-		getPropertyDetail();
-	}, []);
-	const locationUrl = useLocation();
-	return (
-		<HeaderStyle color={color}>
-			<div className='header-logo'>
-				{locationUrl.pathname === "/terms" ? (
-					<img src={Logo2} alt='Logo' />
-				) : locationUrl.pathname === "/privacy" ? (
-					<img src={Logo2} alt='Logo' />
-				) : (
-					<img src={Logo} alt='Logo' />
-				)}
-				<div className='menu-icon' onClick={handleClick}>
-					<i className={click ? "fas fa-times" : "fas fa-bars"} />
-				</div>
-			</div>
-			<div className={click ? "header-nav actived" : "header-nav"}>
-				<NavLink to='/' onClick={closeMobileMenu}>
-					Home
-				</NavLink>
-				<NavLink to='/about' onClick={closeMobileMenu}>
-					About
-				</NavLink>
-				{propertyData && (
-					<div
-						onMouseEnter={onMouseEnter}
-						onMouseLeave={onMouseLeave}
-						className='dropdown'>
-						<NavLink to='#'>
-							Properties <i className='fas fa-caret-down' />
-						</NavLink>
-						{dropdown && (
-							<div className='dropdown-menu'>
-								{propertyData.map((item, key) => {
-									return (
-										<NavLink
-											to='/property'
-											onClick={() => {
-												window.localStorage.setItem(
-													"Properties",
-													JSON.stringify(item)
-												);
-												if (
-													propertyData.length !== 1 &&
-													locationUrl.pathname === "/property"
-												) {
-													window.location.reload();
-												}
-											}}
-											key={key}>
-											{item.first.propertyName}
-										</NavLink>
-									);
-								})}
-							</div>
-						)}
-					</div>
-				)}
-				<NavLink
-					to='/contact'
-					className='nav-links-mobile'
-					onClick={closeMobileMenu}>
-					<Button padding='20px 65px' bg='#bc9144'>
-						Contact
-					</Button>
-				</NavLink>
-			</div>
-			<NavLink to='/contact' className='nav-links'>
-				<Button padding='23px 65px' bg='#bc9144'>
-					Contact
-				</Button>
-			</NavLink>
-		</HeaderStyle>
-	);
+  const onMouseLeave = () => {
+    if (window.innerWidth > 1600) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+  useEffect(() => {
+    getPropertyDetail();
+  }, []);
+  const locationUrl = useLocation();
+  return (
+    <HeaderStyle color={color}>
+      <div className="header-logo">
+        {locationUrl.pathname === "/terms" ? (
+          <img src={Logo2} alt="Logo" />
+        ) : locationUrl.pathname === "/privacy" ? (
+          <img src={Logo2} alt="Logo" />
+        ) : (
+          <img src={Logo} alt="Logo" />
+        )}
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+      </div>
+      <div className={click ? "header-nav actived" : "header-nav"}>
+        <NavLink to="/" onClick={closeMobileMenu}>
+          Home
+        </NavLink>
+        <NavLink to="/about" onClick={closeMobileMenu}>
+          About
+        </NavLink>
+        {propertyData && (
+          <div
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            className="dropdown"
+          >
+            <NavLink to="#">
+              Properties <i className="fas fa-caret-down" />
+            </NavLink>
+            {dropdown && (
+              <div className="dropdown-menu">
+                {propertyData.map((item, key) => {
+                  return (
+                    <NavLink
+                      to="/property"
+                      onClick={() => {
+                        window.localStorage.setItem(
+                          "Properties",
+                          JSON.stringify(item)
+                        );
+                        if (
+                          propertyData.length !== 1 &&
+                          locationUrl.pathname === "/property"
+                        ) {
+                          window.location.reload();
+                        }
+                      }}
+                      key={key}
+                    >
+                      {item.first.propertyName}
+                    </NavLink>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        )}
+        <NavLink
+          to="/contact"
+          className="nav-links-mobile"
+          onClick={closeMobileMenu}
+        >
+          <Button padding="20px 65px" bg="#bc9144">
+            Contact
+          </Button>
+        </NavLink>
+      </div>
+      <NavLink to="/contact" className="nav-links">
+        <Button padding="23px 65px" bg="#bc9144">
+          Contact
+        </Button>
+      </NavLink>
+    </HeaderStyle>
+  );
 };
 
 export default Header;
--- src/components/newdetails/fifthNew.js
+++ src/components/newdetails/fifthNew.js
@@ -10,161 +10,162 @@ import { db } from "../../utils/firebase-config";
 import { ref, set, onValue } from "firebase/database";
 
 const FifthNew = () => {
-	// const [uploadIcon, setUploadIcon] = useState("");
-	// const [iconName, setIconName] = useState("");
-	// const formIcon = new FormData();
-	// formIcon.append("file", uploadIcon);
-	// formIcon.append("upload_preset", "brookehowse");
+  // const [uploadIcon, setUploadIcon] = useState("");
+  // const [iconName, setIconName] = useState("");
+  // const formIcon = new FormData();
+  // formIcon.append("file", uploadIcon);
+  // formIcon.append("upload_preset", "brookehowse");
 
-	const [amenitiesData, setAmenitiesData] = useState("");
+  const [amenitiesData, setAmenitiesData] = useState("");
 
-	useEffect(() => {
-		onValue(ref(db), (snapshot) => {
-			const data = snapshot.val();
-			if (data !== null) {
-				setAmenitiesData(Object.values(data.newAmenity));
-			}
-			console.log(amenitiesData);
-		});
-	}, []);
+  useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      const data = snapshot.val();
+      if (data !== null) {
+        setAmenitiesData(Object.values(data.newAmenity));
+      }
+      console.log(amenitiesData);
+    });
+  }, []);
 
-	const [uploadImage, setUploadImage] = useState("");
-	const [isLoaded, setIsLoaded] = useState(false);
-	const amenitiesDetails = localStorage.getItem("Amenities");
-	// const [amenitiesDetail, setAmenitiesDetail] = useState(() => {
-	// 	if (amenitiesDetails === "undefined") {
-	// 		return [];
-	// 	} else if (amenitiesDetails === null) {
-	// 		return [];
-	// 	} else {
-	// 		return JSON.parse(amenitiesDetails);
-	// 	}
-	// });
+  const [uploadImage, setUploadImage] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const amenitiesDetails = localStorage.getItem("Amenities");
+  // const [amenitiesDetail, setAmenitiesDetail] = useState(() => {
+  // 	if (amenitiesDetails === "undefined") {
+  // 		return [];
+  // 	} else if (amenitiesDetails === null) {
+  // 		return [];
+  // 	} else {
+  // 		return JSON.parse(amenitiesDetails);
+  // 	}
+  // });
 
-	const [amenitiesDetail, setAmenitiesDetail] = useState([]);
+  const [amenitiesDetail, setAmenitiesDetail] = useState([]);
 
-	useEffect(() => {}, [amenitiesDetail]);
-	const {
-		setUploadImage5,
-		uploadButton5,
-		uploadImage5,
-		setUploadButton5,
-		setLatestAmenitiesDetail,
-	} = useContext(PropertiesContext);
+  useEffect(() => {}, [amenitiesDetail]);
+  const {
+    setUploadImage5,
+    uploadButton5,
+    uploadImage5,
+    setUploadButton5,
+    setLatestAmenitiesDetail,
+  } = useContext(PropertiesContext);
 
-	const formData = new FormData();
-	formData.append("file", uploadImage);
-	formData.append("upload_preset", "brookehowse");
+  const formData = new FormData();
+  formData.append("file", uploadImage);
+  formData.append("upload_preset", "brookehowse");
 
-	const editProperty = localStorage.getItem("editProperty");
-	const editPropertyCont = JSON.parse(editProperty);
-	useEffect(() => {
-		if (uploadButton5 === true && uploadImage5 === "") {
-			setUploadButton5(false);
-		}
-	}, []);
-	return (
-		<>
-			<ToastContainer />
-			<p className='details'>
-				Please provide required details <span>- Section 5</span>
-			</p>
-			<div className='new-body'>
-				<div>
-					{uploadImage5 ? (
-						<div className='new-body-img'>
-							<Image cloudName='brookehowse' publicId={uploadImage5} />
-						</div>
-					) : (
-						uploadImage && (
-							<div className='new-body-img'>
-								<img src={URL.createObjectURL(uploadImage)} alt='house' />
-							</div>
-						)
-					)}
+  const editProperty = localStorage.getItem("editProperty");
+  const editPropertyCont = JSON.parse(editProperty);
+  useEffect(() => {
+    if (uploadButton5 === true && uploadImage5 === "") {
+      setUploadButton5(false);
+    }
+  }, []);
+  return (
+    <>
+      <ToastContainer />
+      <p className="details">
+        Please provide required details <span>- Section 5</span>
+      </p>
+      <div className="new-body">
+        <div>
+          {uploadImage5 ? (
+            <div className="new-body-img">
+              <Image cloudName="brookehowse" publicId={uploadImage5} />
+            </div>
+          ) : (
+            uploadImage && (
+              <div className="new-body-img">
+                <img src={URL.createObjectURL(uploadImage)} alt="house" />
+              </div>
+            )
+          )}
 
-					<label className='new-label'>
-						<input
-							type='file'
-							onChange={(e) => {
-								if (editPropertyCont) {
-									setUploadImage5("");
-									setUploadImage(e.target.files[0]);
-									setUploadButton5(true);
-								} else {
-									setUploadImage(e.target.files[0]);
-									setUploadButton5(true);
-								}
-							}}
-						/>
-						Change Image
-					</label>
-					<div className='upload-btns'>
-						{uploadButton5 &&
-							(isLoaded ? (
-								<Loader />
-							) : (
-								<Button
-									padding='19px 50px'
-									bg='#bc9144'
-									onClick={async () => {
-										setIsLoaded(true);
-										try {
-											const response = await axios.post(
-												"https://api.cloudinary.com/v1_1/brookehowse/upload",
-												formData
-											);
-											setUploadImage5(response.data.secure_url);
-											window.scrollTo(0, 0);
-											toast.success("Photo Updated Successfully!!!!");
-											setIsLoaded(false);
-											setUploadButton5(false);
-										} catch (error) {
-											toast.error(error.message);
-											setIsLoaded(false);
-											setUploadButton5(true);
-										}
-									}}>
-									Upload
-								</Button>
-							))}
-					</div>
-				</div>
-				<div className='new-amenities'>
-					<p>
-						Amenities <span>(max of 6)</span>
-					</p>
-					<div className='amenities-wrapper'>
-						{amenitiesData &&
-							amenitiesData?.map((item, key) => {
-								return (
-									<div className='amenity-wrapper' key={key}>
-										<div>
-											<input
-												type='checkbox'
-												onChange={(e) => {
-													if (e.target.checked) {
-														let tempAmenitiesDetails = [
-															...amenitiesDetail,
-															item,
-														];
-														setAmenitiesDetail(tempAmenitiesDetails);
-														setLatestAmenitiesDetail(tempAmenitiesDetails);
-													} else {
-														let temp = amenitiesDetail.splice(key, 1);
-														setAmenitiesDetail(temp);
-														setLatestAmenitiesDetail(temp);
-													}
-													console.log(amenitiesDetail);
-												}}
-											/>
-											<p>{item.amenityName}</p>
-										</div>
-									</div>
-								);
-							})}
-					</div>
-					{/* <form
+          <label className="new-label">
+            <input
+              type="file"
+              onChange={(e) => {
+                if (editPropertyCont) {
+                  setUploadImage5("");
+                  setUploadImage(e.target.files[0]);
+                  setUploadButton5(true);
+                } else {
+                  setUploadImage(e.target.files[0]);
+                  setUploadButton5(true);
+                }
+              }}
+            />
+            Change Image
+          </label>
+          <div className="upload-btns">
+            {uploadButton5 &&
+              (isLoaded ? (
+                <Loader />
+              ) : (
+                <Button
+                  padding="19px 50px"
+                  bg="#bc9144"
+                  onClick={async () => {
+                    setIsLoaded(true);
+                    try {
+                      const response = await axios.post(
+                        "https://api.cloudinary.com/v1_1/brookehowse/upload",
+                        formData
+                      );
+                      setUploadImage5(response.data.secure_url);
+                      window.scrollTo(0, 0);
+                      toast.success("Photo Updated Successfully!!!!");
+                      setIsLoaded(false);
+                      setUploadButton5(false);
+                    } catch (error) {
+                      toast.error(error.message);
+                      setIsLoaded(false);
+                      setUploadButton5(true);
+                    }
+                  }}
+                >
+                  Upload
+                </Button>
+              ))}
+          </div>
+        </div>
+        <div className="new-amenities">
+          <p>
+            Amenities <span>(max of 6)</span>
+          </p>
+          <div className="amenities-wrapper">
+            {amenitiesData &&
+              amenitiesData?.map((item, key) => {
+                return (
+                  <div className="amenity-wrapper" key={key}>
+                    <div>
+                      <input
+                        type="checkbox"
+                        onChange={(e) => {
+                          if (e.target.checked) {
+                            let tempAmenitiesDetails = [
+                              ...amenitiesDetail,
+                              item,
+                            ];
+                            setAmenitiesDetail(tempAmenitiesDetails);
+                            setLatestAmenitiesDetail(tempAmenitiesDetails);
+                          } else {
+                            let temp = amenitiesDetail.splice(key, 1);
+                            setAmenitiesDetail(temp);
+                            setLatestAmenitiesDetail(temp);
+                          }
+                          console.log(amenitiesDetail);
+                        }}
+                      />
+                      <p>{item.amenityName}</p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+          {/* <form
 						onSubmit={(e) => {
 							e.preventDefault();
 							if (amenitiesDetail === null) {
@@ -191,7 +192,7 @@ const FifthNew = () => {
 							}}
 						/>
 					</form> */}
-					{/* <div className='amenities-detail'>
+          {/* <div className='amenities-detail'>
 						{amenitiesDetail?.map((el, key, index) => {
 							return (
 								<div
@@ -209,8 +210,8 @@ const FifthNew = () => {
 							);
 						})}
 					</div> */}
-				</div>
-				{/* <div>
+        </div>
+        {/* <div>
 					<label className='new-label'>
 						<input
 							type='file'
@@ -255,9 +256,9 @@ const FifthNew = () => {
 						Upload
 					</button>
 				</div> */}
-			</div>
-		</>
-	);
+      </div>
+    </>
+  );
 };
 
 export default FifthNew;
